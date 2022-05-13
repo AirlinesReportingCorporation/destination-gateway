044d6a1bcd7b0f2936a04c6f405c09478206d422
Ian Fajardo <ianfajardozx@gmail.com>
2022-05-13 13:22:32 -0400

    init commit

---
+++ src/app.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="arc-thirdparty-page">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">Replace content here</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
